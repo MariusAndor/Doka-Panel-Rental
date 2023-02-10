7a1f0a5c99675b665bcee596249606dcfc2156e0
Holso <109223950+Holso@users.noreply.github.com>
2023-02-10 11:40:34 +0200

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,23 @@
+let i=1;
+  function afisare(){
+    let humburger = document.getElementById("nav");
+    if(i==1){
+      humburger.classList.add("activare");
+      i=2;
+      console.log(i);
+    }else if(i==2){
+      humburger.classList.remove("activare");
+      i=1;
+    }
+  }
+  
+  window.addEventListener('scroll', function change(){
+    let navbar = document.getElementById("nav-bar");
+    if(window.scrollY >= 165){
+      navbar.classList.remove("headerColorOut");
+      navbar.classList.add("headerColorOn");
+    }else if(window.scrollY < 165){
+      navbar.classList.remove("headerColorOn");
+      navbar.classList.add("headerColorOut");
+    }
+  });
